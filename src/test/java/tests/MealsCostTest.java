c4873698b1fc0b11d5ae79297d3ef211a9a7ebaf
kaiseristvan96 <kaiseristvan96@gmail.com>
2019-05-15 08:32:35 +0200

    Added a few more unit tests

--- src/test/java/tests/MealsCostTest.java
+++ src/test/java/tests/MealsCostTest.java
@@ -6,24 +6,49 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class MealsCostTest {
     Meal meal = new Meal("pizza1",100);
+    Meal meal2 = new Meal("pizza2",1000);
+    Meal meal3 = new Meal("pizza2",0);
     @Test
-    void TaxTest(){
+    void taxTestWithHundred(){
         assertEquals(127,meal.costWithTax(meal));
     }
 
     @Test
-    void TaxTestFail(){
+    void taxTestWithThousand(){
+        assertEquals(1270,meal2.costWithTax(meal2));
+    }
+
+    @Test
+    void taxTestWithNull(){
+        assertEquals(0,meal3.costWithTax(meal3));
+    }
+
+
+    @Test
+    void taxTestFail(){
         assertNotEquals(126,meal.costWithTax(meal));
     }
 
 
     @Test
-    void ConvertToEURTest(){assertEquals(
+    void convertToEURTestHundred(){assertEquals(
             true
             ,(meal.costInEuro(meal) >= (meal.getCost()/323.51)-0.01 ) && (meal.costInEuro(meal) <= (meal.getCost()/323.51)+0.01 ));}
 
     @Test
-    void ConvertToEURTestFails(){assertEquals(
+    void convertToEURTestThousand(){assertEquals(
+            true
+            ,(meal2.costInEuro(meal2) >= (meal2.getCost()/323.51)-0.01 ) && (meal2.costInEuro(meal2) <= (meal2.getCost()/323.51)+0.01 ));}
+
+
+
+    @Test
+    void convertToEURNull(){assertEquals(
+            true
+            ,(meal3.costInEuro(meal3) >= (meal3.getCost()/323.51)-0.01 ) && (meal3.costInEuro(meal3) <= (meal3.getCost()/323.51)+0.01 ));}
+
+    @Test
+    void convertToEURTestFails(){assertEquals(
             false
             ,(meal.getCost()/323.51) == 0.30);}
 
